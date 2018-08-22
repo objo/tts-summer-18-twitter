3d74054c51944133d0d51dbabe31d5ab46306b4a
Joe OBrien <joe@objo.com>
2018-08-22 18:40:56 -0400

    created nav elements and user pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,8 @@ class UsersController < ApplicationController
     @users = User.all - [current_user]
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "timeline#index"
-  get "/timeline" => "timeline#index"
-
-  get "/users" => "users#index"
+  get "/timeline" => "timeline#index", as: :timeline
+  get "/users" => "users#index", as: :users
+  get "/users/:id" => "users#show", as: :user
 end
